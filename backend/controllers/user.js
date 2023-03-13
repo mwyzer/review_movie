4099e4820713e602a08dae075a8fd58966453f16
mwyzer <muhammad.wyzer@gmail.com>
2023-03-13 21:03:45 +0700

    verification token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,10 +1,69 @@
-const User = require('../models/user')
+const nodemailer = require('nodemailer');
+const User = require('../models/user');
+const EmailVerificationToken = require('../models/emailVerificationToken');
+const { isValidObjectId } = require('mongoose');
 
 exports.create = async (req, res) => {
-  const { name, email, password } = req.body
+  const { name, email, password } = req.body;
 
-  const newUser = new User({ name, email, password })
-  await newUser.save()
+  const oldUser = await User.findOne({ email });
+  if (oldUser)
+    return res.status(401).json({ error: 'This email is already in used!' });
 
-  res.json({ user: newUser })
+  const newUser = new User({ name, email, password });
+  await newUser.save();
+
+  // generate 6 digit otp
+  let OTP = '';
+  for (let i = 0; i <= 5; i++) {
+    const randomVal = Math.round(Math.random() * 9);
+    OTP += randomVal;
+  }
+  // store otp inside db
+  const newEmailVerificationToken = new EmailVerificationToken({
+    owner: newUser._id,
+    token: OTP,
+  });
+
+  await newEmailVerificationToken.save();
+
+  // send OTP to user
+
+  var transport = nodemailer.createTransport({
+    host: 'sandbox.smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: '2b7c3d2ffa290a',
+      pass: '714f615059092c',
+    },
+  });
+
+  transport.sendMail({
+    from: 'verification@reviewapp.com',
+    to: newUser.email,
+    subject: 'Email Verification',
+    html: `
+      <p>Your verification OTP</p>
+      <h1>${OTP}</h1>
+    `,
+  });
+
+  res.status(201).json({
+    message:
+      'Please verify your email. OTP has been sent to your email account!',
+  });
+};
+
+exports.verifyEmail = async (req, res) => {
+  const { userId, OTP } = req.body;
+
+  if (isValidObjectId(userId)) return res.json({ error: 'Invalid User!' });
+
+  const user = await User.findById(userId);
+  if (!user) return res.json({ error: 'user not found!' });
+
+  if (user.isVerified) return res.json({ error: 'user is already verified!' });
+
+  const token = await EmailVerificationToken.findOne({ owner: userId });
+  if (!token) return res.json({ error: 'token not found' });
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { check } = require('express-validator');
 const { create } = require('../controllers/user');
+const { userValidator, validate } = require('../middleware/validator');
 
 const router = express.Router();
 
-router.post('/create', create);
+router.post('/create', userValidator, validate, create);
 
 module.exports = router;
